d2340e9c0e39d3c5693239a0f4b544208d0a920c
Feroze <ysuf.feroze@yahoo.com>
2023-05-30 20:13:10 +0200

    Le dés marche

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,22 @@
-// Lancer de dés
+// Fonction pour Lancer les dés
 
-let numbersDecimal = (Math.random() * 6) + 1;
-let numbers = Math.floor(numbersDecimal);
+    // Donner un nombre aléatoir entre 1 et 6
+        const throwDice = () => {
+            let numbersDecimal = (Math.random() * 6) + 1;
+            let numbers = Math.floor(numbersDecimal);
+            return numbers;
+        };
+
+    // Afficher le résultat 
+        const displayResult = () => {
+            const result = throwDice();
+            const image = document.getElementById('dice');
+            image.src = '/Dés/dé ' + result + '.png'
+        }; 
+
+    // Affecter un évenement lors du click sur le boutton 
+        const diceButton = document.getElementById('throw');
+        diceButton.addEventListener('click', displayResult);
+
 
 
