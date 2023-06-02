c8d93efe868f2c0bb0e43e67480750d4bc52a64c
Feroze <ysuf.feroze@yahoo.com>
2023-06-02 16:12:15 +0200

    Probleme probleme

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,27 +14,26 @@
             image.src = '/Dés/dé ' + result + '.png';
         }; 
 
-    // Affecter un évenement lors du click sur le boutton 
-        const diceButton = document.getElementById('throw');
-        diceButton.addEventListener('click', displayResult);
-
 // Fonction Nouvelle Partie demande de choisir un identifiant
 
     // Créer des pop_up pour ecrire un identifiant
         const modal = document.querySelector('.modal');
-        const definePlayers = () => {
+
+        const startNewGame = () => {
             modal.classList.toggle('active');
-        };
+        }
 
-        const closePopUp = document.getElementById('closePop');
-
-
-        const startNewGame =  document.getElementById('nouvelle_partie');
-        startNewGame.addEventListener('click', definePlayers);
-        closePopUp.addEventListener('click', definePlayers);
+        const closePop = () => {
 
 
 
+
+
+
+
+            
+            modal.classList.toggle('disactive');
+        }
 
 
 
