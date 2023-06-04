289436d250fbe078272d6ae98aebc1cc9602356f
Feroze <ysuf.feroze@yahoo.com>
2023-06-04 16:20:57 +0200

    Les regle du jeux sont ous appliqué et le pop gagne marche manque Fermer le pop gagnant

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,43 +1,70 @@
-// Definition du tour du joueur
+// Bouton Hold
+    const turnOne = document.querySelector('.turn_one');
+    const turnTwo = document.querySelector('.turn_two');
 
-    // Déclarer une fonction tour du joueur 
-        const turnOne = document.querySelector('.turn_one');
-        const turnTwo = document.querySelector('.turn_two');
+    const globalOne = document.getElementById('global_one');
+    const globalTwo = document.getElementById('global_two');
 
-        const globalOne = document.getElementById('global_one');
-        const globalTwo = document.getElementById('global_two');
+    const userOne = document.getElementById('user_one');
+    const userTwo = document.getElementById('user_two');
 
-        let playerOne = true;
-        let playerTwo = false;
+    const endMessage = document.getElementById('end_message');
+    const winnerPopFermer = document.getElementById('fermer');
+    const endGame = document.querySelector('.end_game');
 
-        let playerOneGlobalScore = 0;
-        let playerTwoGlobalScore = 0;
+    let playerOne = true;
+    let playerTwo = false;
 
-        const turnPlayers = () => {
+    let playerOneGlobalScore = 0;
+    let playerTwoGlobalScore = 0;
 
-            if (playerOne === true) {
-                playerOneGlobalScore += firstPlayer;
-                firstPlayer = 0;
-                currentOne.innerText = firstPlayer;
-                globalOne.innerText = playerOneGlobalScore
-            }
-            if (playerTwo === true){
-                playerTwoGlobalScore += secondPlayer;
-                secondPlayer = 0;
-                currentTwo.innerText = secondPlayer;
-                globalTwo.innerText = playerTwoGlobalScore
-            }
-
-            if (getComputedStyle(turnOne).display != "none" && getComputedStyle(turnTwo).display == "none") {
-                playerOne = (turnOne.style.display = "none") && false;
-                playerTwo = (turnTwo.style.display = "inline") && true;
-            } else {
-                playerOne = (turnOne.style.display = "inline") && true;
-                playerTwo = (turnTwo.style.display = "none") && false;
-            }
+    const turnPlayers = () => {
+        // Permet de stocker le score
+        if (playerOne === true) {
+            playerOneGlobalScore += firstPlayer;
+            firstPlayer = 0;
+            currentOne.innerText = firstPlayer;
+            globalOne.innerText = playerOneGlobalScore
+        }
+        if (playerTwo === true){
+            playerTwoGlobalScore += secondPlayer;
+            secondPlayer = 0;
+            currentTwo.innerText = secondPlayer;
+            globalTwo.innerText = playerTwoGlobalScore
         }
 
-// Fonction pour Lancer les dés et affichet le résultat dans la case Points
+        // Regle pour ne pas dépasser 100 points
+        if (playerOneGlobalScore > 100) {
+            playerOneGlobalScore = 0;
+            globalOne.innerText = playerOneGlobalScore
+        };
+        if (playerTwoGlobalScore > 100) {
+            playerTwoGlobalScore= 0;
+            globalTwo.innerText = playerTwoGlobalScore
+        };
+
+        // Permet de changer de joueur
+        if (getComputedStyle(turnOne).display != "none" && getComputedStyle(turnTwo).display == "none") {
+            playerOne = (turnOne.style.display = "none") && false;
+            playerTwo = (turnTwo.style.display = "inline") && true;
+        } else {
+            playerOne = (turnOne.style.display = "inline") && true;
+            playerTwo = (turnTwo.style.display = "none") && false;
+        }
+
+        // Affiche le winner
+        if (playerOneGlobalScore === 100) {
+            endGame.classList.toggle('active');
+            endMessage.innerText = `${userOne} à gagner la partie`;
+            winnerPopFermer.classList.toggle('active');
+        } else if (playerTwoGlobalScore === 100) {
+            endGame.classList.toggle('active');
+            endMessage.innerText = `${userTwo} à gagner la partie`;
+            winnerPopFermer.classList.toggle('active');
+        }
+    }
+
+// Bouton Dés
 
     const currentOne = document.getElementById('current_one');
     const currentTwo = document.getElementById('current_two');
@@ -45,43 +72,41 @@
     let secondPlayer = 0;
     let firstPlayer = 0;
 
-    // Afficher le résultat && S’il obtient un 1, c’est la fin de son tour. A faire son score ROUND est perdu.
-        const displayResult = () => {
-
-            let numbersDecimal = (Math.random() * 6) + 1;
-            let numbers = Math.floor(numbersDecimal);
-            const result = numbers;
-            const image = document.getElementById('dice');
-            image.src = '/Dés/dé ' + result + '.png';
-
-                if (playerOne === true) {
-                    secondPlayer = 0;
-                    currentTwo.innerText = secondPlayer;
-                    firstPlayer += result;
-                    currentOne.innerText = firstPlayer;
-                } else {
+   
+    const displayResult = () => {
+        // Calcul et renvoie un chiffre aléatoire entre 1 à 6
+        let numbersDecimal = (Math.random() * 6) + 1;
+        let numbers = Math.floor(numbersDecimal);
+        const result = numbers;
+        const image = document.getElementById('dice');
+        image.src = '/Dés/dé ' + result + '.png';
+        // Affiche le résultat dans la case POINTS
+            if (playerOne === true) {
+                secondPlayer = 0;
+                currentTwo.innerText = secondPlayer;
+                firstPlayer += result;
+                currentOne.innerText = firstPlayer;
+            } else {
+                firstPlayer = 0;
+                currentOne.innerText = firstPlayer;
+                secondPlayer += result;
+                currentTwo.innerText = secondPlayer;
+            };
+            // Si le score est équivaut à 1 les points sont perdu et c'est le joueur suivant qui joue
+            if (result === 1) {
+                if (getComputedStyle(turnOne).display != "none" && getComputedStyle(turnTwo).display == "none") {
+                    playerOne = (turnOne.style.display = "none") && false;
                     firstPlayer = 0;
                     currentOne.innerText = firstPlayer;
-                    secondPlayer += result;
+                    playerTwo = (turnTwo.style.display = "inline") && true;
+                } else {
+                    playerOne = (turnOne.style.display = "inline") && true;
+                    playerTwo = (turnTwo.style.display = "none") && false;
+                    secondPlayer = 0;
                     currentTwo.innerText = secondPlayer;
-                };
-
-                if (result === 1) {
-                    if (getComputedStyle(turnOne).display != "none" && getComputedStyle(turnTwo).display == "none") {
-                        playerOne = (turnOne.style.display = "none") && false;
-                        firstPlayer = 0;
-                        currentOne.innerText = firstPlayer;
-                        playerTwo = (turnTwo.style.display = "inline") && true;
-                    } else {
-                        playerOne = (turnOne.style.display = "inline") && true;
-                        playerTwo = (turnTwo.style.display = "none") && false;
-                        secondPlayer = 0;
-                        currentTwo.innerText = secondPlayer;
-                    }
                 }
-        }; 
-
-
+            }
+    }; 
 
 // Fonction Nouvelle Partie demande de choisir un identifiant
 
@@ -105,8 +130,8 @@
         const btnValider = document.getElementById('valider');
 
         btnValider.addEventListener('click', () => {
-            user_one.innerText = inputOne.value
-            user_two.innerText = inputTwo.value
+            userOne.innerText = inputOne.value
+            userTwo.innerText = inputTwo.value
         })
             
 
